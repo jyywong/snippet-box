aa4f07b680ca55fa1c52fad489b12d3df109b9bb
jyywong <wong.jonathan1@gmail.com>
2024-08-21 17:24:15 -0400

    Move routes to diff file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,21 +15,13 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	app := &application{
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
-	mux := http.NewServeMux()
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	app.logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	app.logger.Error(err.Error())
 	os.Exit(1)
 }
